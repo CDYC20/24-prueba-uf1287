14d32d07bc7784a71149ee0ec4abb6e3955c8958
CDYC20 <cristydyc.20@gmail.com>
2024-06-18 13:43:05 +0200

    /suma

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const app = express()
 const port = 3000
 
 
-app.get('/hola', (req, res) => {
-    res.send('Hello Worldd')
-})
 
+function sumar(sum1, sum2) {
+    sum1 = Math.floor(Math.random ()*50)
+    sum2 = Math.floor(Math.random ()*50)
+    var resultado = [sum1 + sum2];
+    console.log(sum1 + "+" + sum2 + "=" + resultado)  
+    return sum1, sum2, resultado
+}
+
+app.get('/suma', (req, res) => {
+    sum1 = req
+    sum2 = req
+    res.send(sumar(sum1, sum2))
+})
+    
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
