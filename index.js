6e9f341fe53f8494af20966e6d08081ce17f6ce3
CDYC20 <cristydyc.20@gmail.com>
2024-06-18 10:56:23 +0200

    /holaa

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static("public/")) 
 
 app.get('/hola', (req, res) => {
     res.send('Hello Worldd')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
   
