a56b78f54177d322102b9f34ea4cf7705c53c645
CDYC20 <cristydyc.20@gmail.com>
2024-06-19 12:51:26 +0200

    /suma

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get('/hola', (req, res) => {
     res.send("Hello Worldd")
 })
 
+const sumar = (req, res) => {
+    sum1 = Number(req.query.sumando1)
+    sum2 = Number(req.query.sumando2)
+    var resultado = sum1 + sum2
+
+    res.send(`sumar: ${sum1} + ${sum2} = ${resultado}`)
+}
+app.get('/suma', sumar)
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
